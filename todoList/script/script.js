3fea0b0f05f00daafc47bd827759425914635b9d
Victor Reis <31421139+vic-reis@users.noreply.github.com>
2023-02-18 00:48:00 -0300

    vish

--- todoList/script/script.js
+++ todoList/script/script.js
@@ -1,2 +1,14 @@
-const texto=document.querySelector("#texto");
-const adicionar=document.querySelector(".adicionar");
+// Selecione o elemento de input de texto
+const input = document.querySelector('input[type="text"]');
+
+// Crie um array vazio
+let array = [];
+
+// Adicione um ouvinte de eventos para o botão
+document.querySelector('button').addEventListener('click', () => {
+  // Adicione o valor do input ao final do array
+  array.push(input.value);
+
+  // Mostre o array no console
+  console.log(array);
+});
