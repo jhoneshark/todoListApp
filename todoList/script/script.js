adf9026229a316992cf826e1daba239d18a776c6
Victor Reis <31421139+vic-reis@users.noreply.github.com>
2023-02-18 18:28:38 -0300

    criadas 5 funções para melhor manutenção

--- todoList/script/script.js
+++ todoList/script/script.js
@@ -7,69 +7,113 @@ GUIA
   [x] adicionar o botão de editar
   [ ] adicionar verificação de entrada vazia
   [ ]
+
+REFATORAÇÃO
+  [ ] Dividir os eventos em funções menores
+  [ ] Usar nomes de variáveis descritivos
+  [ ] Adicionar comentários explicativos
+  [ ] Usar recursos de linguagem modernos: arrow functions, templates literals, spread operators e destructuring
+  [ ] Implementação de TDD
+        - adição
+        - exclusão
+        - edição
+        - adição de subtarefa
+        - foco no campor de entrada
+  [ ] Uso de bibliotecas*
 */
-const input = document.querySelector('input[type="text"]');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
-let array = [];
 
-button.addEventListener('click', () => {
-  if (input.value.trim() !== '') {
-    array.push(input.value);
-    input.value = '';
 
-    const div = document.createElement('div');
+const inputField = document.querySelector('input[type="text"]');
+const addButton = document.querySelector('button');
+const listContainer = document.querySelector('#list');
+let taskArray = [];
 
-    const span = document.createElement('span');
-    span.textContent = array[array.length - 1];
-    div.appendChild(span);
+function addTask() {
+  const taskInput = inputField.value.trim();
 
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Excluir';
-    buttonDelete.addEventListener('click', () => {
-      div.remove();
-      array.splice(array.indexOf(span.textContent), 1);
-    });
-    div.appendChild(buttonDelete);
+  if (taskInput !== '') {
+    taskArray.push(taskInput);
+    inputField.value = '';
 
-    const buttonEdit = document.createElement('button');
-    buttonEdit.textContent = 'Editar';
-    buttonEdit.addEventListener('click', () => {
-      const text = prompt("Digite o novo texto para o item da lista:", span.textContent);
-      if (text !== null) {
-        span.textContent = text;
-        array[array.indexOf(span.textContent)] = text;
-      }
-    });
-    div.appendChild(buttonEdit);
+    const taskDiv = createTaskElement(taskInput);
+    listContainer.appendChild(taskDiv);
+  }
+}
 
-    const buttonAddSubtask = document.createElement('button');
-    buttonAddSubtask.textContent = 'Adicionar Subtarefa';
-    buttonAddSubtask.addEventListener('click', () => {
-      const subtaskText = prompt("Digite o texto da subtarefa:");
-      if (subtaskText !== null && subtaskText.trim() !== '') {
-        const subtaskSpan = document.createElement('span');
-        subtaskSpan.textContent = '- ' + subtaskText;
-        div.appendChild(subtaskSpan);
-      }
-    });
-    div.appendChild(buttonAddSubtask);
+function createTaskElement(taskText) {
+  const taskDiv = document.createElement('div');
+  const taskTextSpan = document.createElement('span');
+  const deleteButton = createTaskButton('Excluir', deleteTask);
+  const editButton = createTaskButton('Editar', editTask);
+  const addSubtaskButton = createTaskButton('Adicionar Subtarefa', addSubtask);
 
-    div.addEventListener('click', () => {
-      input.focus();
-    });
+  taskTextSpan.textContent = taskText;
 
-    list.appendChild(div);
+  taskDiv.appendChild(taskTextSpan);
+  taskDiv.appendChild(deleteButton);
+  taskDiv.appendChild(editButton);
+  taskDiv.appendChild(addSubtaskButton);
+
+  taskDiv.addEventListener('click', () => {
+    inputField.focus();
+  });
+
+  return taskDiv;
+}
+
+function createTaskButton(buttonText, eventFunction) {
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  button.addEventListener('click', eventFunction);
+
+  return button;
+}
+
+function deleteTask() {
+  const taskDiv = this.parentElement;
+  const taskIndex = taskArray.indexOf(taskDiv.querySelector('span').textContent);
+
+  taskDiv.remove();
+  taskArray.splice(taskIndex, 1);
+}
+
+function editTask() {
+  const taskDiv = this.parentElement;
+  const taskTextSpan = taskDiv.querySelector('span');
+  const newText = prompt("Digite o novo texto para o item da lista:", taskTextSpan.textContent);
+
+  if (newText !== null) {
+    taskTextSpan.textContent = newText;
+    taskArray[taskArray.indexOf(taskTextSpan.textContent)] = newText;
+  }
+}
+
+function addSubtask() {
+  const taskDiv = this.parentElement;
+  const subtaskText = prompt("Digite o texto da subtarefa:");
+
+  if (subtaskText !== null && subtaskText.trim() !== '') {
+    const subtaskSpan = document.createElement('span');
+    subtaskSpan.textContent = '- ' + subtaskText;
+    taskDiv.appendChild(subtaskSpan);
+  }
+}
+
+addButton.addEventListener('click', addTask);
+
+inputField.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    if (document.activeElement === inputField) {
+      event.preventDefault();
+      addButton.click();
+    } else {
+      inputField.focus();
+    }
   }
 });
 
-input.addEventListener('keydown', (event) => {
+document.querySelector('body').addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    if (document.activeElement === input) {
-      event.preventDefault();
-      button.click();
-    } else {
-      input.focus();
-    }
+    inputField.focus();
   }
 });
