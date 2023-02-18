466942731371b7420e8527f0064dde1a482e4273
Victor Reis <31421139+vic-reis@users.noreply.github.com>
2023-02-18 09:45:52 -0300

    att

--- todoList/script/script.js
+++ todoList/script/script.js
@@ -1,43 +1,66 @@
-/** 
-             uma preguiça do karai de comentar tudo isso kkkkkk, mas no resumo..
-        
-             o primeiro bloco eu estou referenciando os elementos da DOM no JS
-             
-             o segundo bloco eu adiciono um evento ouvinte no botão e quando ele é clicado chama uma função anonima q:
-                - pega o valor da entrada de texto e joga no final do array
-                - limpa o texto na entrada de texto
-                
-             o terceiro bloco cria uma LI, 
-             depois pega o ultimo valor do array e através do método "textContent" adiciona na LI 
-             e por ultimo, adiciono a LI gerada no UL que é referenciado pelo id="lista"
-            **/
+/* 
+GUIA
+  [x] gerar a entrada de texto e o botão para adicionar as tarefas
+  [x] adicionar os valores da entrada em um array
+  [x] gerar uma lista com os elemementos do array
+  [x] adicionar o botão de excluir
+  [x] adicionar o botão de editar
+  [ ] adicionar verificação de entrada vazia
+  [ ]
+*/
+
+// seleciona o elemento <input> do tipo texto
 const input = document.querySelector('input[type="text"]');
+// seleciona o botão da página
 const button = document.querySelector('button');
+// seleciona o elemento <ul> (lista não ordenada) da página
 const list = document.querySelector('#list');
+// cria uma matriz vazia para armazenar os itens da lista
 let array = [];
 
+// adiciona um "event listener" (ouvidor de eventos) ao botão
 button.addEventListener('click', () => {
+  // adiciona o valor do elemento <input> à matriz de itens
   array.push(input.value);
+  // limpa o valor do elemento <input>
   input.value = '';
 
-  const li = document.createElement('li');
-  li.textContent = array[array.length - 1];
+  // cria uma nova <div> para o novo item da lista
+  const div = document.createElement('div');
+  
+  // cria um <span> para exibir o texto do item da lista
+  const span = document.createElement('span');
+  span.textContent = array[array.length - 1];
+  // adiciona o <span> à <div>
+  div.appendChild(span);
+  
+  // cria um botão "Excluir" para remover o item da lista
+  const buttonDelete = document.createElement('button');
+  buttonDelete.textContent = 'Excluir';
+  buttonDelete.addEventListener('click', () => {
+      // remove a <div> do item da lista
+      div.remove(); 
+      // remove o item da matriz de itens
+      array.splice(array.indexOf(span.textContent), 1); 
+  });
+  // adiciona o botão "Excluir" à <div>
+  div.appendChild(buttonDelete);
+  
+  // cria um botão "Editar" para editar o texto do item da lista
+  const buttonEdit = document.createElement('button');
+  buttonEdit.textContent = 'Editar';
+  buttonEdit.addEventListener('click', () => {
+      // exibe um prompt para o usuário editar o texto do item da lista
+      const text = prompt("Digite o novo texto para o item da lista:", span.textContent);
+      // se o usuário clicar em OK e digitar um novo texto, atualiza o texto do <span> e da matriz de itens
+      if (text !== null) {
+        span.textContent = text;
+        array[array.indexOf(span.textContent)] = text;
+      }
+  });
+  // adiciona o botão "Editar" à <div>
+  div.appendChild(buttonEdit);
 
-  let imgDelete = document.createElement('img');
-  imgDelete.src = 'imgs/delete.png'
-
-  let imgEdit = document.createElement('img');
-  imgEdit.src = 'imgs/edit.png'
-
-    // Adiciona um botão ao elemento li para permitir a exclusão
-    const button = document.createElement('button');
-    button.textContent = 'Excluir';
-    button.addEventListener('click', () => {
-        li.remove(); // Remove o elemento li correspondente
-        array.splice(array.indexOf(li.textContent), 1); // Remove o valor do array correspondente
-    });
-
-  list.appendChild(li);
-  list.appendChild(imgDelete);
-  list.appendChild(imgEdit);
+  // adiciona a <div> à lista não ordenada
+  list.appendChild(div);
 });
